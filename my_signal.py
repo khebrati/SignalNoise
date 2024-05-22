cd7cd5d7ef966e0d9f953b8de9cb424564d44a0e
Mohammad Erfan Khebrati <khebrati@gmail.com>
2024-05-22 10:41:42 +0330

    some refactoring

--- my_signal.py
+++ my_signal.py
@@ -2,8 +2,8 @@ import sympy as sp
 from sympy import symbols, floor
 import matplotlib.pyplot as plt
 
-def signal(V, frequency):
+def signal(v, frequency):
     t = symbols('t')
-    signal = V * (-1)**floor(2*frequency*t)
+    signal = v * (-1)**floor(2*frequency*t)
     return signal
 
--- plot_signal.py
+++ plot_signal.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from my_signal import signal
 
+frequency = 10e6
 
-
-t_values = np.linspace(0, 10e-6, 1000) 
-signal_values = [signal().subs('t', t_val) for t_val in t_values]
+signal_t_points = np.linspace(0, 10/frequency, 2000)
+signal_values = [signal(v = 5,frequency=frequency).subs('t', t_val) for t_val in signal_t_points]
 
 
 plt.figure(figsize=(10, 6))
-plt.plot(t_values, signal_values)
+plt.plot(signal_t_points, signal_values)
 plt.title('Digital Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Voltage (V)')
